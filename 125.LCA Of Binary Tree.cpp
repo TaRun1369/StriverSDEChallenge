2f3615458108eaf9d71f9929ec35e6382eb768d4
Tarun Santani <98692373+TaRun1369@users.noreply.github.com>
2023-08-01 10:31:38 +0530

    Update 125.LCA Of Binary Tree.cpp

--- 125.LCA Of Binary Tree.cpp
+++ 125.LCA Of Binary Tree.cpp
@@ -1,26 +1,35 @@
-int dfs(BinaryTreeNode<int> *root)
-{
-    if(root->left==NULL and root->right==NULL) return root->data;
-    int leftt=0, rightt=0;
-    if(root->left) leftt=dfs(root->left);
-    if(root->right) rightt=dfs(root->right);
-    if(leftt+rightt>=root->data) return root->data = leftt+rightt;
-    // if(root->left and root->right) 
-    // {
-    //     root->left->data = (root->data+1)/2;
-    //     root->right->data = root->data/2;
-    // }
-    if(root->left) root->left->data = root->data;
-    if(root->right) root->right->data = root->data;
-    if(root->left) leftt=dfs(root->left);
-    if(root->right) rightt=dfs(root->right);
-    return root->data = leftt+rightt;
+TreeNode <int> * lca(TreeNode<int> *root, int x, int y){
+
+    if(root==NULL or root->data==x or root->data==y) return root;
+
+ 
+
+    TreeNode<int>* leftTree = lca(root->left,x,y);
+
+    TreeNode<int>* rightTree = lca(root->right,x,y);
+
+ 
+
+    if(leftTree==NULL) return rightTree;
+
+    if(rightTree==NULL) return leftTree;
+
+ 
+
+    return root;
 
 }
-void changeTree(BinaryTreeNode < int > * root) {
-    // Write your code here.
-    if(root==NULL) return;
-    int k = dfs(root);
-    // pre(root);
 
-}  
+ 
+
+int lowestCommonAncestor(TreeNode<int> *root, int x, int y)
+
+{
+
+    //    Write your code here
+
+    TreeNode <int> *ans=lca(root, x, y);
+
+    return ans->data;   
+
+}
